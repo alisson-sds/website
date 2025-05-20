bd92a601f6e36df755425cd151a48ba6d159dafb
Alisson Silveira <alisson-sds@hotmail.com>
2025-05-20 17:34:25 -0300

    Design da navbar, inicio da home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NgOptimizedImage, NgStyle, NgClass } from '@angular/common'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, NgOptimizedImage, NgStyle, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { NgOptimizedImage } from '@angular/common'
+
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
