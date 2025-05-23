58b21fdb290bef627b49f51730422bc7185e4be8
Alisson Silveira <alisson-sds@hotmail.com>
2025-05-23 17:54:51 -0300

    Mudança no título da main

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
-import { NgOptimizedImage, NgStyle } from '@angular/common'
-
 @Component({
   selector: 'app-navbar',
-  imports: [NgOptimizedImage, NgStyle],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
