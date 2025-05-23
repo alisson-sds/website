7f41ef3887a6ce7b5115eeeb74869a573b3e3b64
Alisson Silveira <alisson-sds@hotmail.com>
2025-05-23 13:54:35 -0300

    Escolha do gradient e melhora na navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NgOptimizedImage, NgStyle, NgClass } from '@angular/common'
+import { NgOptimizedImage, NgClass } from '@angular/common'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, NgOptimizedImage, NgStyle, NgClass],
+  imports: [RouterOutlet, NavbarComponent, NgOptimizedImage, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
