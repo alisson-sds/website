c97ed252a4a2e9a91725b3724387cf9633f14a7a
Alisson Silveira <alisson-sds@hotmail.com>
2025-05-23 22:06:04 -0300

    Melhora na home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgOptimizedImage, NgClass, NgStyle } from '@angular/common'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, NgOptimizedImage, NgClass, NgStyle],
+  imports: [RouterOutlet, NavbarComponent, NgOptimizedImage, NgClass, NgStyle, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
